d80014469e898cadf2cbde6b064a58a9478df9e8
Angel Solano <angelsolano@MacBook-Air.local>
2025-11-24 11:18:32 -0600

    Ajuste en font-size global, se agrega aviso de privacidad (es,en), navbar breadcrumbs y toggle menu background

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const replacements = {
-    "[brand]": "<span>Zhongbang Fine Spinning</span>",
-    "[experience]": "<span>más de 10 años de experiencia</span>",
-    "[Intelligent R&D:]": "<span>Intelligent R&D</span>"
-};
-
-function applyReplacements(text) {
-    for (const key in replacements) {
-        text = text.replaceAll(key, replacements[key]);
-    }
-    return text;
-}
+const savedLang = localStorage.getItem("lang") || "en";
